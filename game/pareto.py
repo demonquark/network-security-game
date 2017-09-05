be1aa7956c4e68cbcac567f31a17aa45c9acb1e7
xahiru <xahiru@gmail.com>
2017-09-06 03:53:39 +0800

    new pareto logic
    not done yet

--- game/pareto.py
+++ game/pareto.py
@@ -5,18 +5,24 @@ import time
 config = Config()
 state = State(config)
 
-
 def simple_cull(inputPoints, dominates):
     paretoPoints = []#set()
+    actionIndices = []#set()
     candidateRowNr = 0
     #dominatedPoints = set()
+    
+    index = 0
+    
     while True:
         candidateRow = inputPoints[candidateRowNr]
         inputPoints.remove(candidateRow)
         rowNr = 0
         nonDominated = True
+        
+        
         while len(inputPoints) != 0 and rowNr < len(inputPoints):
             row = inputPoints[rowNr]
+            
             #print("row======")
             #print(row)
             #print("candidateRow")
@@ -31,15 +37,20 @@ def simple_cull(inputPoints, dominates):
                 rowNr += 1
             else:
                 rowNr += 1
+                
 
         if nonDominated:
             # add the non-dominated point to the Pareto frontier
-            #paretoPoints.add(tuple(candidateRow))
+            actionIndices.append(index)
             paretoPoints.append(candidateRow)
+            
+        
+
 
         if len(inputPoints) == 0:
             break
-    return paretoPoints
+        
+    return paretoPoints, actionIndices
 
 def dominates(row, candidateRow):
     return sum([row[x] >= candidateRow[x] for x in range(len(row))]) == len(row)  
@@ -75,65 +86,160 @@ def dominates_two(def_row, def_candidateRow):
         
     return dominated
 
-def cull_two(def_paretoPoints):
-    mix_paretoPoints = set()
-    candidateStartRow = 0
-    nonDominated = True
-    while True:
-        currentRow = 0
-        candidateRow = def_paretoPoints[candidateRowNr]
-        def_paretoPoints.remove(candidateRow)
-        setNonDominated = True
-        while len(def_paretoPoints) != 0 and currentRow < len(def_paretoPoints):
-            row = def_paretoPoints[currentRow]
 
-        if len(def_paretoPoints) == 0:
-            break
-    
-    return mix_paretoPoints
+def is_pareto_efficient(costs):
+    """
+    :param costs: An (n_points, n_costs) array
+    :return: A (n_points, ) boolean array, indicating whether each point is Pareto efficient
+    """
+    is_efficient = np.ones(costs.shape[0], dtype = bool)
+    for i, c in enumerate(costs):
+        #print(2)
+        if is_efficient[i]:
+            is_efficient[is_efficient] = np.any(costs[is_efficient]>=c, axis=1)  # Remove dominated points
+    return is_efficient
+
+
+def all_equal(elements):
+    """
+    :param elements: A collection of things
+    :return: True if all things are equal, otherwise False.  (Note that an empty list of elements returns true, just as all([]) is True
+    """
+    element_iterator = iter(elements)
+    try:
+        first = element_iterator.next() # Will throw exception
+    except StopIteration:
+        return True
+    return all(a == first for a in element_iterator)
+
+
+def find_pareto_ixs(cost_arrays):
+    """
+    :param cost_arrays: A collection of nd-arrays representing a grid of costs for different indices.
+    :return: A tuple of indices which can be used to index the pareto-efficient points.
+    """
+    assert all_equal([c.shape for c in cost_arrays])
+    flat_ixs, = np.nonzero(is_pareto_efficient(np.reshape(cost_arrays, (len(cost_arrays), -1)).T), )
+    ixs = np.unravel_index(flat_ixs, dims=cost_arrays[0].shape)
+    return ixs
+
 
 def prep_perato(attackActions, defenceActions):
     mixInputs = []
     for i in defenceActions:
         tempMix = []
         for j in attackActions:
-            if i!=j:
+            #if i!=j:
                 #tempReward = list(map(sum, zip(state.get_score(),i,j)))
-                tempReward = list(map(sum, zip(i,j)))
-                tempReward[2] = i[2]-j[2]
+                #tempReward = list(map(sum, zip(i,j)))
+                #tempReward[2] = i[2]-j[2]
                 #mixInputs.add(tuple(tempReward))
-                tempMix.append(tempReward)
+            tempMix.append(j)
+                
         #print (tempMix)
-        paretoPoints = simple_cull(tempMix, dominates)
+
+        paretoPoints, actionIndices = simple_cull(tempMix, dominates)
+        #paretoPoints  = is_pareto_efficient(np.array(tempMix))
+
         #print (paretoPoints)
+        #print (actionIndices)
         mixInputs.append(paretoPoints)
+        #mixInputs.append(np.array(tempMix))
         
         
     return mixInputs
 
 
+
+def prep_pareto_efficient(attackActions,defenceActions): #input is a numpy array of attacks for each diffence strategy
+    mixInputs = np.zeros(defenceActions.shape[0],defenceActions.shape)
+    mixIndices = 0
+
+    for i in enumerate(defenceActions):
+        
+        mixInputs[i,:,:] = attackActions
+        #paretoPoints  = is_pareto_efficient(np.array(tempMix))
+
+        #print (paretoPoints)
+        #print (actionIndices)
+        #mixInputs.append(paretoPoints)
+
+    return mixIndices
+
+
+
 import random
 
 
-#inputPoints = [[random.randint(70,100) for i in range(3)] for j in range(10)]
-attackActions = [[random.randint(70,100) for i in range(3)] for j in range(100)]
-defenceActions = [[random.randint(70,100) for i in range(3)] for j in range(100)]
-#print(inputPoints)
-#paretoPoints, dominatedPoints = simple_cull(inputPoints, dominates)
+attackActions = [[random.randint(70,100) for i in range(3)] for j in range(1000)]
+defenceActions = [[random.randint(70,100) for i in range(3)] for j in range(1000)]
+
+attackActions2 = attackActions[:]
+defenceActions2 = defenceActions[:]
+
+
+inputPoints = [[random.randint(70,100) for i in range(3)] for j in range(1000)]
+inputPoints2 = inputPoints[:]
+
+inp = np.array(inputPoints2)
+
+
+prep_pareto_efficient
+
+print(inputPoints)
+print("===")
+print(inputPoints)
+
+print("==simple_cull=")
+start_time = time.time()
+paretoPoints, actionIndices = simple_cull(inputPoints, dominates)
+print(paretoPoints)
+print ("--- %s seconds ---" % (time.time() - start_time))
+
+
+print("==is_pareto_efficient=")
+
+
+#print(inp)
+start_time = time.time()
+poindex = is_pareto_efficient(np.array(inputPoints2))
+print(inp[poindex])
+print ("--- %s seconds ---" % (time.time() - start_time))
+
+
+
+
 
 #print("===")
 #print(list(paretoPoints))
-print("prep===")
-start_time = time.time()
-mixInputs = prep_perato(attackActions,defenceActions)
+#print("===")
+#print(actionIndices)
 
+
+#mixInputs = prep_perato(attackActions,defenceActions)
+#mixInputs2 = mixInputs[:]
+
+#print("==mixInuptold=")
+#start_time = time.time()
 #print(mixInputs)
+#simple_cull(mixInputs, dominates_two)
 
-paretoPoints = simple_cull(mixInputs, dominates_two)
 #print("final===")
 #print(paretoPoints)
+#print ("--- %s seconds ---" % (time.time() - start_time))
 
-print ("--- %s seconds ---" % (time.time() - start_time))
+
+print("==mixefficent_findpareto=")
+#start_time = time.time()
+#print(mixInputs)
+#find_pareto_ixs(np.array(mixInputs2))
+
+#print("final===")
+#print(paretoPoints)
+#print ("--- %s seconds ---" % (time.time() - start_time))
+
+
+
 #import matplotlib.pyplot as plt
 #from mpl_toolkits.mplot3d import Axes3D
 
